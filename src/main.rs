bb933ee4e9315d93696b50a415ecdfabf857a809
df@ <@>
2022-06-07 14:08:27 +0100

    ok

--- src/main.rs
+++ src/main.rs
@@ -19,12 +19,12 @@ use std::io::{stdout, Write};
 // use std::io::{Write, };
 use crossterm::{queue, style::Print};
 
-type RawField = [[usize; WIDTH]; HEIGHT];
-type SolvedField = [[Cell; WIDTH]; HEIGHT];
+type RawField = [[usize; HEIGHT]; WIDTH];
+type SolvedField = [[Cell; HEIGHT]; WIDTH];
 
-const WIDTH: usize = 6;
-const HEIGHT: usize = 4;
-const MINE_PROBABILITY: f32 = 0.9;
+const WIDTH: usize = 18;
+const HEIGHT: usize = 9;
+const MINE_PROBABILITY: f32 = 0.97;
 const NEIGHBOURS: [(i32, i32); 9] = [
     (-1, -1), (0, -1), (1, -1),
     (-1,  0), (0,  0), (1,  0),
@@ -80,8 +80,8 @@ fn main() {
 
     let field = solve_field(field);
 
-    assert_eq!(field.len(), HEIGHT);
-    assert_eq!(field[0].len(), WIDTH);
+    assert_eq!(field.len(), WIDTH);
+    assert_eq!(field[0].len(), HEIGHT);
 
     run(field, mine_count);
 }
@@ -89,15 +89,22 @@ fn main() {
 fn run(mut field: SolvedField, mut mine_count: u32) -> crossterm::Result<()> {
     crossterm::terminal::enable_raw_mode();
     let mut stdout = stdout();
-    let (sx, sy) = (0, 0);
+    // stdout.queue(crossterm::terminal::Clear{clea})
+    let (sy, sx) = (0, 0);
     loop {
+        stdout.queue(crossterm::terminal::Clear(crossterm::terminal::ClearType::All))?;
         for (y, row) in field.iter().enumerate() {
             let y = y as u16;
             for (x, cell) in row.iter().enumerate() {
                 let x = x as u16;
+                if y == sy && x == sx {
+                    // stdout.queue(crossterm::style::SetBackgroundColor(crossterm::style::Color::Green))?;
+                    stdout.queue(crossterm::style::SetForegroundColor(crossterm::style::Color::Red))?;
+                }
                 stdout
                     .queue(crossterm::cursor::MoveTo(y, x))?
-                    .queue(Print(cell.cell_type))?;
+                    .queue(Print(cell.cell_type))?
+                    .queue(crossterm::style::ResetColor)?;
             }
             println!();
         }
@@ -114,10 +121,10 @@ fn run(mut field: SolvedField, mut mine_count: u32) -> crossterm::Result<()> {
 
 fn generate_field() -> (RawField, u32) {
     let mut rng = rand::thread_rng();
-    let mut field: RawField = [[0; WIDTH]; HEIGHT];
+    let mut field: RawField = [[0; HEIGHT]; WIDTH];
     let mut mine_count = 0;
-    for y in 0..HEIGHT {
-        for x in 0..WIDTH {
+    for y in 0..WIDTH {
+        for x in 0..HEIGHT {
             if rng.gen::<f32>() > MINE_PROBABILITY {
                 field[y][x] = 1;
                 mine_count += 1;
@@ -130,16 +137,16 @@ fn generate_field() -> (RawField, u32) {
 }
 
 fn solve_field(field: RawField) -> SolvedField {
-    let mut newfield: SolvedField = [[Cell::new(); WIDTH]; HEIGHT];
-    for y in 0..HEIGHT {
-        for x in 0..WIDTH {
+    let mut newfield: SolvedField = [[Cell::new(); HEIGHT]; WIDTH];
+    for y in 0..WIDTH {
+        for x in 0..HEIGHT {
             if field[y][x] == 1 {
                 newfield[y][x].cell_type = FieldType::Mine;
             } else {
                 let mut mine_count = 0;
                 for neighbour in NEIGHBOURS {
                     let (ny, nx) = (y as i32 + neighbour.0, x as i32 + neighbour.1);
-                    if ny < 0 || ny > HEIGHT as i32 - 1 || nx < 0 || nx > WIDTH as i32 - 1 {
+                    if ny < 0 || ny > WIDTH as i32 - 1 || nx < 0 || nx > HEIGHT as i32 - 1 {
                         continue;
                     }
                     let (ny, nx) = (ny as usize, nx as usize);
